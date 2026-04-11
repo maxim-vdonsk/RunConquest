20d50cb19bb159d7e8c1930c49519602f57194e6
maxim_vdonsk <maxim_vdonsk@mail.ru>
2026-04-11 19:49:56 +0300

    Initial Commit

---
+++ RunConquest/RunConquestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunConquestApp.swift
+//  RunConquest
+//
+//  Created by Максим Шурховецкий on 11.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct RunConquestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
